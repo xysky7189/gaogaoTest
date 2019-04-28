844f54b5b4422ad876dd1bc1378b7f328ffc47fe
xindiyang <2425509077@qq.com>
2019-04-28 17:24:14 +0800

    除了懒加载功能，其余功能已经实现

--- banquanqukuanlian/js/index.js
+++ banquanqukuanlian/js/index.js
@@ -7,64 +7,64 @@ var datas = [{
         banquan: '版权家',
         zh: '账户',
         time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-},
-{
-    hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
-    banquan: '版权家',
-    zh: '账户',
-    time: '2019 - 04 - 24 06: 55: 34'
-}
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    },
+    {
+        hash: 'd3544c8c7c194bead6808cbd4d1be59ebbe2c37bc85e760c731794820f775eb8',
+        banquan: '版权家',
+        zh: '账户',
+        time: '2019 - 04 - 24 06: 55: 34'
+    }
 
 ];
-
+//通过for循环进行动态加载表格
 for (var i = 0; i < datas.length; i++) {
     var tbody = $('tbody');
     var strHtml = `
@@ -77,3 +77,18 @@ for (var i = 0; i < datas.length; i++) {
 `;
     $(strHtml).appendTo(tbody);
 }
+
+//点击tab进行页面切换
+$('#liebiao li').click(function () {
+    $(this).addClass('active').siblings().removeClass('active');
+    var num = $(this).index();
+    $('.content').children('div').eq(num).css('display', 'block').siblings().css('display', 'none');
+});
+
+//点击二维码弹出二维码
+$('#erwei').mouseenter(function () {
+    $('.erma').css('display','block');
+});
+$('#erwei').mouseout(function () {
+    $('.erma').css('display','none');
+});
